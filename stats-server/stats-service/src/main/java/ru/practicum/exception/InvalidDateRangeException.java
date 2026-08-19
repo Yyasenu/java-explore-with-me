8e92e5b3865117bbaf14e0018d40a46394aa7299
Алина <yyasenu@mail.ru>
2026-08-19 16:01:21 +0300

    3 этап

---
+++ stats-server/stats-service/src/main/java/ru/practicum/exception/InvalidDateRangeException.java
@@ -0,0 +1,7 @@
+package main.java.ru.practicum.exception;
+
+public class InvalidDateRangeException extends RuntimeException {
+    public InvalidDateRangeException(String message) {
+        super(message);
+    }
+}
